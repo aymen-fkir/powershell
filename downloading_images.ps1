57207ece4909651e5405155027390b857f809ef4
aymen <aymenfkir23@gmail.com>
2023-03-24 19:50:49 +0100

    Add files via upload

---
+++ downloading_images.ps1
@@ -0,0 +1,12 @@
+$url = Read-Host -Prompt "enter your URL"
+
+$req = Invoke-WebRequest -Uri $url
+
+$images = $req.Images.src
+$wc = New-Object System.Net.WebClient
+$count = 0
+
+foreach($image in $images){
+    $wc.DownloadFile($image,".\images\$count.jpg")
+    $count +=1
+}
